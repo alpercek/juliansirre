c3e00bfe7a01824b1158483cad11da220a11052a
John Doe <alpercekinmez7@gmail.com>
2023-08-22 01:59:29 -0700

    svg

--- slices/S3366/index.vue
+++ slices/S3366/index.vue
@@ -16,20 +16,20 @@ defineProps(
 </script>
 
 <template>
-  <div class="flex">
+  <div :class="{'flex-row-reverse': slice.variation === 'swappedL66R33'}" class="flex gap-5">
     <div class="w-1/3">
   <PrismicImage :field="slice.primary.l33"/>
     </div>
-  <carousel :wrapAround="true" class="w-2/3">
+  <carousel v-if="slice.items.length > 1" :wrapAround="true" class="w-2/3 group">
     <slide v-for="(item, i) in slice.items" :key="`slice-item-${i}`">
       <PrismicImage :field="item.r66" />
     </slide>
 
     <template #addons>
-      <navigation />
-      <pagination />
+      <navigation class="group-hover:opacity-100 opacity-0 transition-all"/>
     </template>
   </carousel>
+  <div v-else><PrismicImage :field="slice.items[0].r66" /></div>
   </div>
 </template>
 <script lang="ts">
@@ -43,3 +43,28 @@ export default {
   },
 }
 </script>
+<style>
+:root{
+--vc-clr-primary: #C7C7C7;
+--vc-nav-color-hover: #C7C7C7;
+}
+.carousel__icon{
+  display: none;
+}
+.carousel__next{
+  border: solid;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 9px;
+    transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+}
+.carousel__prev{
+  border: solid;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 9px;
+    transform: rotate(135deg);
+    -webkit-transform: rotate(135deg);
+}
+</style>
