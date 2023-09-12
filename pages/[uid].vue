787180ba5882611a7ebc6fc59e390754be789904
John Doe <alpercekinmez7@gmail.com>
2023-09-12 03:59:19 -0700

    pagination

--- pages/[uid].vue
+++ pages/[uid].vue
@@ -18,16 +18,18 @@ useHead({
 
 <template>
   <div>
-   <carousel v-if="page?.data.slider.length > 1" :wrapAround="true" :autoplay="5000"  class="w-screen">
-    <slide v-for="(item, i) in page?.data.slider" :key="`page?.data.slider-${i}`">
-      <div><PrismicImage :field="item.image" class="w-screen"/>
-        <div class="group-hover:opacity-100 opacity-0 transition-all flex font-metrik text-[0.625rem] pt-1.5 pr-6 justify-end"><PrismicRichText :field="item.fig"/></div> 
+    <button id="topitop" class="text-[2rem] fixed duration-1000 bottom-28 md:bottom-1/3 right-0 md:right-7 transition-all opacity-0 z-50" @click="toTop()">∴</button>
+   <carousel v-if="page?.data.slider.length > 1" :wrapAround="true" :autoplay="5000" :itemsToShow="1.5" :snapAlign="'start'" class="w-screen">
+    <slide v-for="(item, i) in page?.data.slider" :key="`page?.data.slider-${i}`" class="">
+      <div><PrismicImage :field="item.image" class="object-contain"/>
+        <div class="font-metrik text-[0.625rem] pt-1.5 pr-6 justify-end"><PrismicRichText :field="item.fig"/></div> 
         </div>
       
     </slide>
 
     <template #addons>
-      <navigation class="group-hover:opacity-100 opacity-0 transition-all !hidden md:!flex"/>
+      <navigation class=""/>
+      <Pagination class=""/>
     </template>
   </carousel>
   <SliceZone
@@ -46,5 +48,42 @@ export default {
     Pagination,
     Navigation,
   },
+  methods: {
+   toTop() {
+    window.scrollTo({
+  top: 0,
+  left: 0,
+  behavior: "smooth",
+});
+  }
+  },
 }
 </script>
+<style>
+.carousel__pagination{
+justify-content: flex-start;
+counter-reset: section;
+}
+:root{
+  --vc-pgn-background-color: rgba(255, 0, 0, 0);
+  --vc-pgn-active-color: rgba(255, 255, 0, 0);
+  --vc-pgn-height: 1rem;
+  --vc-pgn-margin: 2px;
+}
+
+.carousel__pagination-button::after {
+  counter-increment: section;
+content: counter(section);
+width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
+  border: solid;
+  border-width: 1px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  font-family: METRIK;
+  font-size: 14px;
+
+}
+</style>
